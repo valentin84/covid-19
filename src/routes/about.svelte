4e409ec4bf14d5a2d04e0adc903a572b7fda273e
Valentin <ciolentin@yahoo.com>
2021-01-31 11:40:18 +0200

    about done

---
+++ src/routes/about.svelte
@@ -0,0 +1,14 @@
+<svelte:head> 
+    <title>
+        About Covid 19
+    </title>
+</svelte:head>
+
+<div class="section header">
+    <div class="container">
+        <h1>About</h1>
+        <p>
+            The data for this site came from <a href="https://covidtracking.com">Covid Tracking</a>. You can learn more about the data by going to their site.
+        </p>
+    </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,6 +4,18 @@
     import TableContainer from '../components/TableContainer.svelte';
 </script>
 
+<svelte:head> 
+    <title>
+        Covid 19 Tracker
+    </title>
+</svelte:head>
+
+<div class="section header">
+    <div class="container">
+        <h1>Covid 19 - US</h1>
+    </div>
+</div>
+
 <CovidStat />
 
 <CovidChart />
