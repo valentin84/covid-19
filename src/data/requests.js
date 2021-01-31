a3d69661d4bd0a1ef810550807cac90a914fe919
Valentin <ciolentin@yahoo.com>
2021-01-31 15:39:12 +0200

    got the api

---
+++ src/data/requests.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import parsers from './parsers';
+
+async function usStats() {
+    const response = await axios.get('https://api.covidtracking.com/v1/us/current.json');
+
+    return parsers.usStats(response.data)
+}
+
+export default {
+    usStats,
+}
