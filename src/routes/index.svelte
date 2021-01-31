a84fe58bd828c5b736aa3b3c6e2cb9aa2a8b338a
Valentin <ciolentin@yahoo.com>
2021-01-31 10:48:22 +0200

    pages done

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,14 @@
+<script>
+    import CovidStat from '../components/CovidStat.svelte';
+    import CovidChart from '../components/CovidChart.svelte';
+    import TableContainer from '../components/TableContainer.svelte';
+</script>
 
+<CovidStat />
+
+<CovidChart />
+
+<TableContainer />
 	
 
 
