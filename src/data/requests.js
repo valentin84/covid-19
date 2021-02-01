ed48b041171748c5d3de94dca2c7e0b5824c9689
Valentin <ciolentin@yahoo.com>
2021-02-01 11:42:38 +0200

    data for states done

--- src/data/requests.js
+++ src/data/requests.js
@@ -7,6 +7,13 @@ async function usStats() {
     return parsers.usStats(response.data)
 }
 
+async function stateStats(state) {
+    const response = await axios.get('https://api.covidtracking.com/v1/states/current.json');
+
+    return parsers.stateStats(state, response.data)
+}
+
 export default {
     usStats,
+    stateStats
 }
--- src/routes/[state].svelte
+++ src/routes/[state].svelte
@@ -1,5 +1,6 @@
 <script context="module" >
     import stateNames from '../data/stateNames.js';
+    import requests from '../data/requests.js';
 
     export async function preload(page) {
         const state = page.params['state'];
@@ -9,7 +10,9 @@
         }
 
         try {
-            return { state: page.params[ 'state' ] }
+            const stats = await requests.stateStats(state);
+
+            return { state, stats }
         } catch(e) {
             this.error(500, "There was an error calling this api, please try again later.");
             return;
@@ -22,7 +25,8 @@
     import CovidChart from '../components/CovidChart.svelte';
     import TableContainer from '../components/TableContainer.svelte';
 
-    export let state
+    export let state;
+    export let stats;
 </script>
 
 <svelte:head> 
@@ -37,6 +41,6 @@
     </div>
 </div>
 
-<CovidStat />
+<CovidStat {...stats} />
 
 <CovidChart />
