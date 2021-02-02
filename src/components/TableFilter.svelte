554c6ddb4280cbfa7314b0d9f53e0703d68b83bf
Valentin <ciolentin@yahoo.com>
2021-02-02 13:42:55 +0200

    app done

--- src/components/TableFilter.svelte
+++ src/components/TableFilter.svelte
@@ -1,1 +1,33 @@
-<h1>Table Filter</h1>
+<script>
+    export let stateName = "";
+    export let sortBy = "name";
+</script>
+   
+<style>
+    input, .select, select {
+        width: 100%;
+    }
+    .column {
+        padding-left: 0;
+    }
+</style>
+
+<div class="section">
+    <div class="container">
+        <div class="columns">
+            <div class="column is-6">
+                <input class="input" type="text" bind:value={stateName} placeholder="Filter States" />
+            </div>
+            <div class="column is-6">
+                <div class="select">
+                    <select bind:value={sortBy} >
+                        <option value="name">By State Name</option>
+                        <option value="cases">Cases</option>
+                        <option value="deaths">Deaths</option>
+                        <option value="tested">Tested</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
